5d76f1d285184d843aed018c01a863dd97653c64
eveconnolly1234 <eve.connolly.1@ucdconnect.ie>
2022-06-30 16:24:27 +0100

    initial commit

--- wedding-isobel-alex/src/App.js
+++ wedding-isobel-alex/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Isobel and Alex wedding
+          Isobel and Alex wedding!!
         </p>
         <a
           className="App-link"
